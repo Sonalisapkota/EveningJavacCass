7a754a55387933490d9089869d4da3edba77c0bd
Sonali Sapkota <sapkotasonali9@gmail.com>
2020-02-12 20:01:47 -0700

    oop in progress

---
+++ src/firstclass/CalculatorJava.java
@@ -0,0 +1,13 @@
+package firstclass;
+public class CalculatorJava {
+
+	public static void main(String[] args) {
+		int a=12,b=10;
+		int sum=a+b;
+		System.out.println("the sum of"+ a +"and"+b+ "is:"+sum);
+		
+	
+
+	}
+
+}
